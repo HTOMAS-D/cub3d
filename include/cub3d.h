9bcdfce97db1ff09efe6fb196d1f6b638a7f6ef8
Ricky <henryats@gmail.com>
2023-05-29 11:12:12 +0100

    🔥Random Makefile Commit🔥

---
+++ include/cub3d.h
@@ -0,0 +1,11 @@
+#ifndef CUB3D_H
+#define CUB3D_H
+
+typedef struct s_cub t_cub;
+
+struct s_cub
+{
+	char *str;
+};
+
+#endif
---
+++ srcs/main.c
@@ -0,0 +1,13 @@
+#include "cub3d.h"
+
+t_cub *cub()
+{
+	static t_cub cub;
+	return(&cub);
+
+}
+
+int main(int ac, char**av)
+{
+	cub();
+}
