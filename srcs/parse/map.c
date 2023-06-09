0390183e1a474c8a267e87339ca8f2e7e4b218c8
Ricky <henryats@gmail.com>
2023-06-09 17:14:48 +0100

    map reading

--- srcs/parse/map.c
+++ srcs/parse/map.c
@@ -1,30 +1,5 @@
 #include "cub3d.h"
 
-// void get_map(t_cub *cub, char *file)
-// {
-// 	char *line = NULL;
-// 	char *temp = malloc(sizeof(char));
-// 	char *temp_free = NULL;
-
-// 	cub->map.fd = open(file, O_RDONLY);
-// 	if (!cub->map.fd)
-// 		ez_exit("Error opening the file");
-// 	temp[0] = '\0';
-// 	while(1)
-// 	{
-// 		line = get_next_line(cub->map.fd);
-// 		if (!line)
-// 			break;
-// 		temp_free = temp;
-// 		printf("%s\n\n", line);
-// 		temp = ft_strjoin(temp, line);
-// 		free(line);
-// 	}
-// 	cub->map.map = ft_split(temp, '\n');
-// 	free(temp);
-// 	free(temp_free);
-// }
-
 int count_map(int fd)
 {
 	char *line;
