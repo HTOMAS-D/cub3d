668ce032c089a13942f6f3e19c51f7296e237096
Ricky <henryats@gmail.com>
2023-06-09 17:22:36 +0100

    main clean

--- srcs/main.c
+++ srcs/main.c
@@ -11,14 +11,5 @@ int main(int ac, char **av)
 {
 	// (void) av;
 	check_file(cub(), ac, av);
-	printf("%s\n", cub()->map.map[0]);
-	printf("%s\n", cub()->map.map[1]);
-	printf("%s\n", cub()->map.map[2]);
-	printf("%s\n", cub()->map.map[3]);
-	printf("%s\n", cub()->map.map[4]);
-	printf("%s\n", cub()->map.map[5]);
-	printf("%s\n", cub()->map.map[6]);
-	printf("%s\n", cub()->map.map[7]);
-	printf("%s\n", cub()->map.map[8]);
 	free_stuff(cub());
 }
