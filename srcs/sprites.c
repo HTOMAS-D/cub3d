ce52cb6a187dfd920716c39d86f5fa78d940a7a7
TiagoTeo <manueltiagoteodoro@gmail.com>
2023-08-17 18:07:56 +0100

    pull conflits

--- srcs/sprites.c
+++ srcs/sprites.c
@@ -6,24 +6,24 @@
 /*   By: mtiago-s <mtiago-s@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/12 18:10:34 by mtiago-s          #+#    #+#             */
-/*   Updated: 2023/08/17 15:47:17 by mtiago-s         ###   ########.fr       */
+/*   Updated: 2023/08/17 17:37:53 by mtiago-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-t_image load_image(char *path)
+t_image	load_image(char *path)
 {
 	t_image	img;
 
 	img.ptr = mlx_xpm_file_to_image(cub()->mlx, path, &img.width, &img.height);
-	img.addr = mlx_get_data_addr(img.ptr, &img.bpp, &img.size_line, &img.endian);
-	return(img);
+	img.addr = mlx_get_data_addr(img.ptr, &img.bpp, &img.size_line, \
+	&img.endian);
+	return (img);
 }
 
 int	init_sprites(t_cub *cub)
 {
-	//printf("%s\n", cub->map.img_paths[1]);
 	cub->wallNO = load_image(cub->map.img_paths[0]);
 	cub->wallSO = load_image(cub->map.img_paths[1]);
 	cub->wallWE = load_image(cub->map.img_paths[2]);
